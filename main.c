65af3a2990509fd24ae0f59fa0cfb61d3bf4c082
Sebastian <sebasrv26@estudiantec.cr>
2020-10-06 17:52:09 -0600

    Reajuste de dimensiones

--- main.c
+++ main.c
@@ -21,10 +21,35 @@ unsigned int sizeText;
 unsigned int size;
 
 /**
+ * Reajuste de la imagem.
+ */
+sfImage* resize(sfImage *originalImage){
+    sfImage *image = NULL;
+    sfSprite *sprite = sfSprite_create();
+    sfTexture *texture = sfTexture_createFromImage(originalImage, NULL);
+
+    sfVector2f vectorRequired = {(float)(1000.0/width), (float)(1000.0/height)};
+    sfSprite_setTexture(sprite, texture, sfTrue);
+    sfSprite_setScale(sprite, vectorRequired);
+    sfFloatRect newSize = sfSprite_getGlobalBounds(sprite);
+    width = (int)newSize.width;
+    height = (int)newSize.height;
+
+    printf("\nSizex: %i, sizey: %i", width, height);
+
+    sfRenderTexture *renderTexture = sfRenderTexture_create(width, height, sfTrue);
+    sfRenderTexture_drawSprite(renderTexture, sprite,NULL);
+    const sfTexture *pTexture = sfRenderTexture_getTexture(renderTexture);
+    image = sfTexture_copyToImage(pTexture);
+    sfImage_flipVertically(image);
+    return image;
+}
+
+/**
  * 1. Carga de imagenes en los formatos: JPG, TIFF y PNG.
  *
- * Resolución mínima:
- * Resolución máxima:
+ * Resolución mínima: 500.
+ * Resolución máxima: 2000.
  * Resize si pasa la resolución:
  * @param path: ruta, nombre y extención de la imagen
  * @return image (sfImage) imagen a utilizar.
@@ -32,22 +57,21 @@ unsigned int size;
 sfImage *loadImage(char *path){
     sfImage* image = NULL;
     image = sfImage_createFromFile(path);
-    sfVector2u size = sfImage_getSize(image);
+    sfVector2u sizes = sfImage_getSize(image);
 
-    width = size.x;
-    height = size.y;
+    width = sizes.x;
+    height = sizes.y;
 
-    int minSize = 500;
-    int maxXSize = 1200;
-    int maxYSize = 1200;
-    printf("\nSizex: %i, sizey: %i", width, height);
+    int minSize = 300; /// 500
+    int maxSize = 1000;
+    printf("\nDimensiones de la imagen original: largo: %i, altura: %i", width, height);
 
     if (width < minSize || height < minSize){
         printf("\nImagen demaciado pequennia");
         return NULL;
-    } else if (width > maxXSize || height > maxYSize) {
-        /// hacerle resize
-        printf("\nHacer Resize");
+    } else if (width > maxSize || height > maxSize) {
+        printf("\nSe hizo un reajuse de dimensiones a %ix%i", width, height);
+        return resize(image);
     }
     return image;
 }
@@ -99,8 +123,10 @@ char* setTypes(int sizeTextP, int typeText){
  * si no dos textos perfectamente… cualquiera de los dos puede venir vacío y por
  * ende no se colocará nada en la imagen). Debe recortarse o restringirse la
  * cantidad de texto en función de las dimensiones de la imagen.
+ *
+ * @param color 0 blanco, 1 negro
  */
-void setText(char *text1, char *text2, struct sfImage *image, char *typeFont){
+void setText(char *text1, char *text2, struct sfImage *image, char *typeFont, int color){
     /// Se crean los objetos de tipo texto y se setean su contenido
     sfText* textoArriba = sfText_create();
     sfText* textoDebajo = sfText_create();
@@ -110,21 +136,24 @@ void setText(char *text1, char *text2, struct sfImage *image, char *typeFont){
     sfTexture *texture = sfTexture_createFromImage(image, NULL); // textura
     sfSprite *sprite = sfSprite_create(); // sprite
 
+    if (color == 1) {
+        sfText_setColor(textoArriba, sfBlack);
+        sfText_setColor(textoDebajo, sfBlack);
+    }
     // (width/(strlen(text1)*0.2))
     /// Establecer la posición del textp
     sfVector2f position1 = {200, 10}; // si el texto es muy largo se corta
-    int pos = height-(height/12);
+    unsigned int pos = height-(height/12);
     if (size == 1){
         pos = height-(height/8);
     }
-    sfVector2f position2 = {200, pos}; // si el texto es muy largo se corta
+    sfVector2f position2 = {200, (float)pos}; // si el texto es muy largo se corta
     sfText_setPosition(textoArriba, position1);
     sfText_setPosition(textoDebajo, position2);
     /// Cambiar la textura de origen de un objeto
     sfSprite_setTexture(sprite, texture, sfTrue);
 
-    printf("\n font %s", typeFont);
-    sfFont * font = sfFont_createFromFile(typeFont); //typeFont
+    sfFont * font = sfFont_createFromFile(typeFont);
     sfText_setCharacterSize(textoArriba, sizeText);
     sfText_setFont(textoArriba, font);
     sfText_setCharacterSize(textoDebajo, sizeText);
@@ -136,21 +165,18 @@ void setText(char *text1, char *text2, struct sfImage *image, char *typeFont){
     sfRenderTexture_drawText(renderTexture, textoDebajo,NULL);
     const sfTexture *pTexture = sfRenderTexture_getTexture(renderTexture);
 
-    sfImage *ima = sfTexture_copyToImage(pTexture);
-    sfImage_flipVertically(ima);
-    saveImage(ima, "C:\\Users\\Usuario\\Downloads\\output.png");
-
+    sfImage *imageWithText = sfTexture_copyToImage(pTexture);
+    sfImage_flipVertically(imageWithText);
+    saveImage(imageWithText, "C:\\Users\\Usuario\\Downloads\\output.png");
 }
 
 int main() {
     /// Paso 1
     sfImage *image = loadImage("C:\\Users\\Usuario\\Downloads\\bugs.jpg");
     /// Paso 2
-    char *typeFont = setTypes(1, 1);
+    char *typeFont = setTypes(0, 0);
     /// Paso 3
-    setText("Memelandia", "Tenemos", image, typeFont);
-    /// Paso 4
-    //saveImage(image, "C:\\Users\\Usuario\\Downloads\\output.png");
+    setText("Memelandia", "Tenemos", image, typeFont, 1);
 
     return 0;
 }
